b6883601c865102fcdb7478d6222fcaf8fe10a11
KARLA INTRIERI ARANTES <25170062@educ123.sp.senai.br>
2025-04-25 08:02:34 -0300

    Atualizando projeto

--- funções/moldura.js
+++ funções/moldura.js
@@ -1,1 +1,29 @@
 //Desenha uma moldura em uma frase
+
+//const readline = require('readline-sync');
+//const frase = readline.question('\nDigite uma frase: ')
+//const ch = '█'
+
+//for (let i=0; i<frase.length+4; i++){
+//   process.stdout.write(ch);
+//}
+
+//console.log(`\n${ch} ${frase} ${ch}`)
+
+//for (let i=0; i<frase.length+4; i++){
+//   process.stdout.write(ch);
+//}
+
+//OU
+{
+const readline = require('readline-sync'); 
+const frase = readline.question('\nDigite uma frase: ') 
+const ch = '█'
+
+function barra (qtde){
+    for (let i=0; i<qtde+4 ; i++){
+        process.stdout.write(ch);
+    }
+    console.log()
+}
+}
